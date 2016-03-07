e774ab0c11cfaa430e840457a08539e6ae4d1fed
TechnoChimp <Tommy.Grissom@gmail.com>
2016-03-07 13:07:13 -0800

    Implemented character selection screen

--- api/routes/game_routes.php
+++ api/routes/game_routes.php
@@ -9,6 +9,81 @@ $app->get('/game', function(RequestInterface $request, ResponseInterface $respon
     return 'Success!';
 });
 
+//////
+// GET /game/character
+
+// Get current user's characters
+$app->get('/game/character', function(RequestInterface $request, ResponseInterface $response) use($app) {
+	// Pull username from auth string and pull characters associated with that username
+	$username = $_SERVER['PHP_AUTH_USER'];
+	$query = "SELECT characters.id as char_id, characters.name as char_name, characters.level as char_level, cities.id as city_id, cities.name as city_name FROM characters LEFT JOIN (users, cities) ON (users.id = characters.user_id AND cities.id = characters.city_id) WHERE users.username = '{$username}'";
+	
+	// Acquire database connection and perform query
+	global $pdo;
+	$statement = $pdo->query($query);
+	$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+	
+	// Return data in JSON format
+	return json_encode($result);
+});
+
+//////
+// GET /game/character/{id}
+
+// Get current character information for specific character
+$app->get('/game/character/{id}', function(RequestInterface $request, ResponseInterface $response) use($app) {
+	// Get character ID from request and username from auth string
+	$id = $request->getAttribute('id');
+	$username = $_SERVER['PHP_AUTH_USER'];
+	
+	// Check if this character belongs to this user
+	$query = "SELECT characters.id as char_id, users.username as user_name FROM characters LEFT JOIN users ON users.id = characters.user_id WHERE characters.id = '{$id}'";
+
+	// Acquire database connection and perform query
+	global $pdo;
+	$statement = $pdo->query($query);
+	$result = $statement->fetch(PDO::FETCH_ASSOC);
+		
+	// If character belongs to this user get all relevant data, otherwise only get publicly available data
+	if (strcasecmp($result["user_name"], $username) != 0) {
+		// Character does not belong to this user (this needs to be built out once user can look up other users' characters)
+		$query = "";
+		return 'This is not your character.';
+	} else {
+		// Character belongs to this user
+		$query = "SELECT c.id as char_id, c.name as user_name, c.level as char_level, cities.id as city_id, cities.name as city_name FROM characters c LEFT JOIN users u ON u.id = c.user_id LEFT JOIN cities	ON cities.id = c.city_id WHERE c.id = '{$id}'";
+		
+		// Perform a new query
+		$statement = $pdo->query($query);
+		$result = $statement->fetch(PDO::FETCH_ASSOC);
+	}
+	
+	// Return data in JSON format
+	return json_encode($result);
+});
+
+//////
+// GET /game/city/{id}
+
+// Get current user's characters
+$app->get('/game/city/{id}', function(RequestInterface $request, ResponseInterface $response) use($app) {
+	// Get city ID from request
+	$id = $request->getAttribute('id');
+
+	$query = "SELECT c.name as city_name FROM cities c WHERE c.id = '{$id}'";
+	
+	// Acquire database connection and perform query
+	global $pdo;
+	$statement = $pdo->query($query);
+	$result = $statement->fetch(PDO::FETCH_ASSOC);
+	
+	// Return data in JSON format
+	return json_encode($result);
+});
+
+//////
+// GET /game/user
+
 // Gathers user information for game initialization (move to new group after figuring out securing multiple groups)
 $app->get('/game/user', function(RequestInterface $request, ResponseInterface $response) use($app) {
 
@@ -24,22 +99,3 @@ $app->get('/game/user', function(RequestInterface $request, ResponseInterface $r
 	return json_encode($result);
 });
 
-// Get current user's characters
-$app->get('/game/character', function(RequestInterface $request, ResponseInterface $response) use($app) {
-	// Pull username from auth string and pull characters associated with that username
-	$username = $_SERVER['PHP_AUTH_USER'];
-	$query = "SELECT characters.id as char_id, characters.name as char_name, characters.level as char_level, cities.name as city_name FROM characters LEFT JOIN (users, cities) ON (users.id = characters.user_id AND cities.id = characters.city_id) WHERE users.username = '{$username}'";
-	
-	// Acquire database connection and perform query
-	global $pdo;
-	$statement = $pdo->query($query);
-	$result = $statement->fetch(PDO::FETCH_ASSOC);
-	
-	// Return data in JSON format
-	return json_encode($result);
-});
-
-// Get current character information for specific character
-$app->get('/game/character/{id}', function(RequestInterface $request, ResponseInterface $response) use($app) {
-	
-});
--- public/js/cityFunctions.js
+++ public/js/cityFunctions.js
@@ -1,5 +1,27 @@
-// 
-function getUserInfo(callback) {
+//////
+// getCharacterData
+
+function getCharacterData(charId) {
+	jQuery.ajax({
+			url: "../api/game/character/"+charId,
+			type: "GET",
+			contentType: 'application/json; charset=utf-8',
+			headers: {'Authorization': 'Basic ' + localStorage.getItem("userAuth")},
+			success: function(resultData) {
+				
+			},
+			error : function(jqXHR, textStatus, errorThrown) {
+			},
+			
+			timeout: 120000,
+		});
+
+}
+
+//////
+// getUserInfo
+
+function getUserInfo() {
 	var result;
 	jQuery.ajax({
 			url: "../api/game/user",
@@ -7,8 +29,7 @@ function getUserInfo(callback) {
 			contentType: 'application/json; charset=utf-8',
 			headers: {'Authorization': 'Basic ' + localStorage.getItem("userAuth")},
 			success: function(resultData) {
-				result = resultData;
-				callback(result);
+				
 			},
 			error : function(jqXHR, textStatus, errorThrown) {
 			},
@@ -22,13 +43,38 @@ function getUserInfo(callback) {
 
 function stageCharacterSelect() {
 	$('#game').empty();
-	$('#game').load('pages/gamesheets/characterselect.html');
+	jQuery.ajax({
+			url: "../api/game/character",
+			type: "GET",
+			contentType: 'application/json; charset=utf-8',
+			headers: {'Authorization': 'Basic ' + localStorage.getItem("userAuth")},
+			success: function(resultData) {
+				// Convert data to javascript object
+				var charData = JSON.parse(resultData);
+				
+				// Load empty character select sheet
+				$('#game').load('pages/gamesheets/characterselect.html', function () {
+					for (i = 0; i < charData.length; i++) {
+						// Load character onto sheet for each character in data
+						$('.charSelect table tr').eq(0).append('<th>'+charData[i].char_name+'</th>');
+						// Load character Id and city Id into DOM for later use
+						$('.charSelect table tr').eq(1).append('<td><div class="character" charId="'+charData[i].char_id+'" cityId="'+charData[i].city_id+'"><img class="charImage" src="img/char1.jpg" /><ul class="charSpec"><li>Level: '+charData[i].char_level+'<li>City: '+charData[i].city_name+'</ul></div></td>');
+					}
+				});
+			},
+			error : function(jqXHR, textStatus, errorThrown) {
+			},
+			
+			timeout: 120000,
+		});
+	
+	
 }
 
 //////
 // stageCity
 
-function stageCity() {
+function stageCity(cityId) {
 	//Set the stage
 	$('#stage').empty();
 	$('#stage').append('<table class="city"><tbody></tbody></table>');
@@ -60,6 +106,28 @@ function stageCity() {
 	
 	//Show the map
 	$('.city').css({"visibility":"visible"});
+	
+	jQuery.ajax({
+			url: "../api/game/city/"+cityId,
+			type: "GET",
+			contentType: 'application/json; charset=utf-8',
+			headers: {'Authorization': 'Basic ' + localStorage.getItem("userAuth")},
+			success: function(resultData) {
+				// Convert data to javascript object
+				var cityData = JSON.parse(resultData);
+
+				// Set the stage title
+				$('#stageTitle').empty();
+				$('#stageTitle').append(cityData.city_name);
+				
+				
+				
+			},
+			error : function(jqXHR, textStatus, errorThrown) {
+			},
+			
+			timeout: 120000,
+		});
 	
 }
 
