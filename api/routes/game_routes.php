9f2444002cef9736ed65d5ab433f4a1b92f05b8f
TechnoChimp <Tommy.Grissom@gmail.com>
2016-04-25 09:04:45 -0700

    Implemented unique inventory and quest journal across multiple characters.

--- api/routes/game_routes.php
+++ api/routes/game_routes.php
@@ -58,13 +58,14 @@ $app->get('/game/character', function(RequestInterface $request, ResponseInterfa
 
 
 //////////////////
-// GET /game/character/inventory
+// GET /game/character/{id}/inventory
 
 // Get current user's characters
-$app->get('/game/character/inventory', function(RequestInterface $request, ResponseInterface $response) use($app) {
+$app->get('/game/character/{id}/inventory', function(RequestInterface $request, ResponseInterface $response) use($app) {
 	// Pull username from auth string and pull characters associated with that username
 	$username = $_SERVER['PHP_AUTH_USER'];
-	$query = "SELECT c.name as item_name, c.description as item_desc, c.icon as item_icon, b.item_count as item_count, b.slot as inv_slot FROM inventory b LEFT JOIN characters a ON a.id = b.character_id LEFT JOIN item c ON c.id = b.item_id WHERE a.name = '{$username}'";
+	$id = $request->getAttribute('id');
+	$query = "SELECT i.name as item_name, i.description as item_desc, i.icon as item_icon, inv.item_count as item_count, inv.slot as inv_slot FROM inventory inv JOIN characters c ON c.id = inv.character_id JOIN item i ON i.id = inv.item_id JOIN users u on c.user_id = u.id WHERE c.id = '{$id}' AND u.username = '{$username}'";
 	
 	// Acquire database connection and perform query
 	global $pdo;
@@ -72,6 +73,103 @@ $app->get('/game/character/inventory', function(RequestInterface $request, Respo
 	$result = $statement->fetchAll(PDO::FETCH_ASSOC);
 	// Return data in JSON format
 	return json_encode($result);
+});
+
+
+
+
+//////////////////
+// GET /game/character/{id}/quest
+
+// Get quest information by quest id
+$app->get('/game/character/{id}/quest', function(RequestInterface $request, ResponseInterface $response) use($app) {
+	// Get username from request
+	$username = $_SERVER['PHP_AUTH_USER'];
+	$id = $request->getAttribute('id');
+	
+	$query = "SELECT q.id as quest_id, q.title as quest_title, qt.type as quest_type, q.description as description, qst.status as status, qs.title as step_title, qs.next_step_id as next_step, gt.type as goal_type, o.name as obj_name, g.quantity as quantity FROM quest q JOIN quest_type qt on q.quest_type_id = qt.id JOIN quest_step qs on q.first_step_id = qs.id JOIN goal g on qs.goal_id = g.id JOIN goal_type gt on g.goal_type_id = gt.id JOIN object o on g.object_id = o.id JOIN character_quest cq on cq.quest_id = q.id JOIN quest_status qst on cq.status_id = qst.id JOIN characters c on cq.char_id = c.id JOIN users u on c.user_id = u.id WHERE c.id = '{$id}' AND u.username = '{$username}'";
+	
+	// Acquire database connection and perform query
+	global $pdo;
+	$statement = $pdo->query($query);
+	$quests = $statement->fetchAll(PDO::FETCH_ASSOC);
+	$questCount = 0;
+	
+	// Loop through each quest
+	foreach ($quests as $quest) {
+		// Form return object
+		// Add first level array indexes
+		$questResult[$questCount]["title"] = $quest["quest_title"];
+		$questResult[$questCount]["type"] = $quest["quest_type"];
+		$questResult[$questCount]["description"] = $quest["description"];
+		$questResult[$questCount]["status"] = $quest["status"];
+		$questResult[$questCount]["steps"] = array();
+		$questResult[$questCount]["rewards"] = array();
+		
+		// Enumerate and add each quest step
+		// Pull first step information from first query
+		$questStep = 0;
+		$questResult[$questCount]["steps"][$questStep] = array(	"step_title" => $quest["step_title"],
+														"step_goal" => $quest["goal_type"],
+														"step_value" => $quest["obj_name"],
+														"step_qty" => $quest["quantity"]
+												);
+		$questStep++;
+		
+		// Check if there is a next step
+		if ($quest["next_step"] != null) {
+			// There are more steps - Perform a new query for the next step
+			$nextStep = $quest["next_step"];
+			
+			do {
+				// Query for the next step
+				$query = "SELECT qs.title as step_title, gt.type as goal_type, o.name as obj_name, g.quantity as quantity, qs.next_step_id as next_step FROM quest_step qs JOIN goal g on qs.goal_id = g.id JOIN goal_type gt on g.goal_type_id = gt.id JOIN object o on g.object_id = o.id WHERE qs.id = '{$nextStep}'";
+				
+				// Acquire database connection and perform query
+				global $pdo;
+				$statement = $pdo->query($query);
+				$step = $statement->fetch(PDO::FETCH_ASSOC);
+				
+				// Add the step to the array
+				$questResult[$questCount]["steps"][$questStep] = array(	"step_title" => $step["step_title"],
+														"step_goal" => $step["goal_type"],
+														"step_value" => $step["obj_name"],
+														"step_qty" => $step["quantity"]
+												);
+				
+				// Check if there is another step
+				$nextStep = $step["next_step"];
+				
+				$questStep++;
+				
+			} while ($nextStep != null);
+		}
+		
+		// Enumerate and add each quest reward
+		$quest_id = $quest["quest_id"];
+		$query = "SELECT qr.id as reward_id, o.name as object_name, o.description as object_desc, ot.type as object_type, of.field as object_field, od.value as field_value, qr.qty as quantity, i.filename as image, s.sprite as sprite FROM quest_reward qr JOIN quest q on qr.quest_id = q.id JOIN quest_type qt on q.quest_type_id = qt.id JOIN object o on qr.object_id = o.id JOIN obj_type ot on o.obj_type_id = ot.id JOIN obj_data od on od.obj_id = o.id JOIN obj_field of on od.field_id = of.id JOIN sprite s on o.sprite_id = s.id JOIN image i on s.image_id = i.id WHERE q.id = '{$quest_id}'";
+		
+		// Acquire database connection and perform query
+		global $pdo;
+		$statement = $pdo->query($query);
+		$rewards = $statement->fetchAll(PDO::FETCH_ASSOC);
+		foreach ($rewards as $reward) {
+			$rewardCount = $reward["reward_id"]-1;
+			$questResult[$questCount]["rewards"][$rewardCount]["object_name"] = $reward["object_name"];
+			$questResult[$questCount]["rewards"][$rewardCount]["object_desc"] = $reward["object_desc"];
+			$questResult[$questCount]["rewards"][$rewardCount]["object_type"] = $reward["object_type"];
+			$questResult[$questCount]["rewards"][$rewardCount]["quantity"] = $reward["quantity"];
+			$questResult[$questCount]["rewards"][$rewardCount]["object_details"][$reward["object_field"]] = $reward["field_value"];
+			$questResult[$questCount]["rewards"][$rewardCount]["image"] = $reward["image"];
+			$questResult[$questCount]["rewards"][$rewardCount]["sprite"] = $reward["sprite"];
+		}
+		
+		// Done with this quest - move on
+		$questCount++;
+	}
+	
+	// Return data in JSON format
+	return json_encode($questResult);
 });
 
 
@@ -210,128 +308,6 @@ $app->get('/game/quest/{id}', function(RequestInterface $request, ResponseInterf
 
 
 //////////////////
-// GET /game/quest
-
-// Get quest information by quest id
-$app->get('/game/quest', function(RequestInterface $request, ResponseInterface $response) use($app) {
-	// Get username from request
-	$username = $_SERVER['PHP_AUTH_USER'];
-	
-	$query = "SELECT q.id as quest_id, q.title as quest_title, qt.type as quest_type, q.description as description, qst.status as status, qs.title as step_title, qs.next_step_id as next_step, gt.type as goal_type, o.name as obj_name, g.quantity as quantity FROM quest q JOIN quest_type qt on q.quest_type_id = qt.id JOIN quest_step qs on q.first_step_id = qs.id JOIN goal g on qs.goal_id = g.id JOIN goal_type gt on g.goal_type_id = gt.id JOIN object o on g.object_id = o.id JOIN character_quest cq on cq.quest_id = q.id JOIN quest_status qst on cq.status_id = qst.id JOIN characters c on cq.char_id = c.id JOIN users u on c.user_id = u.id WHERE u.username = '{$username}'";
-	
-	// Acquire database connection and perform query
-	global $pdo;
-	$statement = $pdo->query($query);
-	$quests = $statement->fetchAll(PDO::FETCH_ASSOC);
-	$questCount = 0;
-	
-	// Loop through each quest
-	foreach ($quests as $quest) {
-		// Form return object
-		// Add first level array indexes
-		$questResult[$questCount]["title"] = $quest["quest_title"];
-		$questResult[$questCount]["type"] = $quest["quest_type"];
-		$questResult[$questCount]["description"] = $quest["description"];
-		$questResult[$questCount]["status"] = $quest["status"];
-		$questResult[$questCount]["steps"] = array();
-		$questResult[$questCount]["rewards"] = array();
-		
-		// Enumerate and add each quest step
-		// Pull first step information from first query
-		$questStep = 0;
-		$questResult[$questCount]["steps"][$questStep] = array(	"step_title" => $quest["step_title"],
-														"step_goal" => $quest["goal_type"],
-														"step_value" => $quest["obj_name"],
-														"step_qty" => $quest["quantity"]
-												);
-		$questStep++;
-		
-		// Check if there is a next step
-		if ($quest["next_step"] != null) {
-			// There are more steps - Perform a new query for the next step
-			$nextStep = $quest["next_step"];
-			
-			do {
-				// Query for the next step
-				$query = "SELECT qs.title as step_title, gt.type as goal_type, o.name as obj_name, g.quantity as quantity, qs.next_step_id as next_step FROM quest_step qs JOIN goal g on qs.goal_id = g.id JOIN goal_type gt on g.goal_type_id = gt.id JOIN object o on g.object_id = o.id WHERE qs.id = '{$nextStep}'";
-				
-				// Acquire database connection and perform query
-				global $pdo;
-				$statement = $pdo->query($query);
-				$step = $statement->fetch(PDO::FETCH_ASSOC);
-				
-				// Add the step to the array
-				$questResult[$questCount]["steps"][$questStep] = array(	"step_title" => $step["step_title"],
-														"step_goal" => $step["goal_type"],
-														"step_value" => $step["obj_name"],
-														"step_qty" => $step["quantity"]
-												);
-				
-				// Check if there is another step
-				$nextStep = $step["next_step"];
-				
-				$questStep++;
-				
-			} while ($nextStep != null);
-		}
-		
-		// Enumerate and add each quest reward
-		$quest_id = $quest["quest_id"];
-		$query = "SELECT qr.id as reward_id, o.name as object_name, o.description as object_desc, ot.type as object_type, of.field as object_field, od.value as field_value, qr.qty as quantity, i.filename as image, s.sprite as sprite FROM quest_reward qr JOIN quest q on qr.quest_id = q.id JOIN quest_type qt on q.quest_type_id = qt.id JOIN object o on qr.object_id = o.id JOIN obj_type ot on o.obj_type_id = ot.id JOIN obj_data od on od.obj_id = o.id JOIN obj_field of on od.field_id = of.id JOIN sprite s on o.sprite_id = s.id JOIN image i on s.image_id = i.id WHERE q.id = '{$quest_id}'";
-		
-		// Acquire database connection and perform query
-		global $pdo;
-		$statement = $pdo->query($query);
-		$rewards = $statement->fetchAll(PDO::FETCH_ASSOC);
-		foreach ($rewards as $reward) {
-			$rewardCount = $reward["reward_id"]-1;
-			$questResult[$questCount]["rewards"][$rewardCount]["object_name"] = $reward["object_name"];
-			$questResult[$questCount]["rewards"][$rewardCount]["object_desc"] = $reward["object_desc"];
-			$questResult[$questCount]["rewards"][$rewardCount]["object_type"] = $reward["object_type"];
-			$questResult[$questCount]["rewards"][$rewardCount]["quantity"] = $reward["quantity"];
-			$questResult[$questCount]["rewards"][$rewardCount]["object_details"][$reward["object_field"]] = $reward["field_value"];
-			$questResult[$questCount]["rewards"][$rewardCount]["image"] = $reward["image"];
-			$questResult[$questCount]["rewards"][$rewardCount]["sprite"] = $reward["sprite"];
-		}
-		
-		// Done with this quest - move on
-		$questCount++;
-	}
-	
-	// Return data in JSON format
-	return json_encode($questResult);
-	
-		
-});
-
-
-
-
-//////////////////
-// User Routes
-
-//////////////////
-// GET /game/user
-
-// Gathers user information for game initialization (move to new group after figuring out securing multiple groups)
-//$app->get('/game/user', function(RequestInterface $request, ResponseInterface $response) use($app) {
-//	// Get username and build query
-//	$username = $_SERVER['PHP_AUTH_USER'];
-//	$query = "SELECT characters.name as char_name, cities.name as city_name FROM characters JOIN users ON users.id = characters.user_id JOIN city_members ON characters.id = city_members.characer_id JOIN cities ON cities.id = city_members.city_id WHERE users.username = '{$username}'";
-//
-//	// Acquire database connection and perform query
-//	global $pdo;
-//	$statement = $pdo->query($query);
-//	$result = $statement->fetch(PDO::FETCH_ASSOC);
-//	
-//	// Return data in JSON format
-//	return json_encode($result);
-//});
-
-
-
-
-//////////////////
 //
 // POST ROUTES
 //
@@ -376,7 +352,7 @@ $app->post('/game/character', function(RequestInterface $request, ResponseInterf
 		// Get user ID and add user to city
 		$charId = $pdo->lastInsertId();
 		
-		$query = "INSERT INTO city_character (char_id, city_id, rank_id) VALUES ('{$charId}', '{$cityId}', '1')";
+		$query = "INSERT INTO city_character (char_id, city_id, rank_id) VALUES ('{$charId}', '{$cityId}', '1'); INSERT INTO inventory (character_id, item_id, item_count, slot) VALUES ('{$charId}', '3', '1', '1'); INSERT INTO character_quest (char_id, quest_id, status_id) VALUES ('{$charId}', '1', '2')";
 		
 		try {
 			global $pdo;
--- js/initialization.js
+++ js/initialization.js
@@ -58,29 +58,30 @@ var city = initObject(city, 1);
 var inv = initObject(invSlot, 60);
 
 // Get inventory data from API and load into objects
-jQuery.ajax({
-			url: "api/game/character/inventory",
-			type: "GET",
-			contentType: 'application/json; charset=utf-8',
-			headers: {'Authorization': 'Basic ' + localStorage.getItem("userAuth")},
-			success: function(resultData) {
-				var inventory = JSON.parse(resultData);
-				for (var i = 0; i < inventory.length; i++) {
-					var slot = inventory[i].inv_slot;
-					var qty = inventory[i].item_count;
-					var name = inventory[i].item_name;
-					var desc = inventory[i].item_desc;
-					var img = inventory[i].item_icon;
-					
-					// Set the data for the current slot
-					inv[slot].setSlot(qty, name, desc, img);
-				}
-			},
-			error: function(jqXHR, textStatus, errorThrown) {
-			},
-			timeout: 120000,
-		});
-
+function initInventory() {
+	jQuery.ajax({
+		url: "api/game/character/"+charId+"/inventory",
+		type: "GET",
+		contentType: 'application/json; charset=utf-8',
+		headers: {'Authorization': 'Basic ' + localStorage.getItem("userAuth")},
+		success: function(resultData) {
+			var inventory = JSON.parse(resultData);
+			for (var i = 0; i < inventory.length; i++) {
+				var slot = inventory[i].inv_slot;
+				var qty = inventory[i].item_count;
+				var name = inventory[i].item_name;
+				var desc = inventory[i].item_desc;
+				var img = inventory[i].item_icon;
+				
+				// Set the data for the current slot
+				inv[slot].setSlot(qty, name, desc, img);
+			}
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+		},
+		timeout: 120000,
+	});
+}
 
 
 
@@ -100,50 +101,52 @@ var items = [];
 // Initialize quest journal
 var questJournal = [];
 
-jQuery.ajax({
-			url: "api/game/quest",
-			type: "GET",
-			contentType: 'application/json; charset=utf-8',
-			headers: {'Authorization': 'Basic ' + localStorage.getItem("userAuth")},
-			success: function(resultData) {
-				var quests = JSON.parse(resultData);
-				
-				
-				// Loop through each quest in result data
-				for (i = 0; i < quests.length; i++) {
-					var type = quests[i].type;
-					var title = quests[i].title;
-					var description = quests[i].description;
-					var status = quests[i].status;
-					var steps = quests[i].steps;
-					var rewards = quests[i].rewards;
-					var questItems = []; // Temporary array to hold each items[] indexed item
-					var questSteps = []; // Temporary array to hold each quest step
+function initQuest() {
+	jQuery.ajax({
+		url: "api/game/character/"+charId+"/quest",
+		type: "GET",
+		contentType: 'application/json; charset=utf-8',
+		headers: {'Authorization': 'Basic ' + localStorage.getItem("userAuth")},
+		success: function(resultData) {
+			var quests = JSON.parse(resultData);
+			
+			
+			// Loop through each quest in result data
+			for (i = 0; i < quests.length; i++) {
+				var type = quests[i].type;
+				var title = quests[i].title;
+				var description = quests[i].description;
+				var status = quests[i].status;
+				var steps = quests[i].steps;
+				var rewards = quests[i].rewards;
+				var questItems = []; // Temporary array to hold each items[] indexed item
+				var questSteps = []; // Temporary array to hold each quest step
 
-					// Create invSlot objects for each reward and add to items array
-					for (j = 0; j < Object.keys(rewards).length; j++) {
-						// Get number of items already in items array
-						// New item index will be this same number since index begins with 0 and length begins with 1
-						var itemIndex = items.length;
+				// Create invSlot objects for each reward and add to items array
+				for (j = 0; j < Object.keys(rewards).length; j++) {
+					// Get number of items already in items array
+					// New item index will be this same number since index begins with 0 and length begins with 1
+					var itemIndex = items.length;
 
-						// Combine image and sprite information
-						var sprite = rewards[j].image.split('.', 1)+'-'+rewards[j].sprite;
+					// Combine image and sprite information
+					var sprite = rewards[j].image.split('.', 1)+'-'+rewards[j].sprite;
 
-						items[itemIndex] = new invSlot('', rewards[j].quantity, rewards[j].object_name, rewards[j].object_desc, sprite);
-						questItems[j] = items[itemIndex];
-					}
-					
-					// Create questStep objects for each quest step and add to questSteps array
-					for (j = 0; j < Object.keys(steps).length; j++) {
-						questSteps[j] = steps[j];
-					}
-					
-					// Create quest object for each quest and add to questJournal array
-					questJournal[i] = new quest(type, title, description, questItems, status, questSteps, i);
+					items[itemIndex] = new invSlot('', rewards[j].quantity, rewards[j].object_name, rewards[j].object_desc, sprite);
+					questItems[j] = items[itemIndex];
 				}
-			},
-			error: function(jqXHR, textStatus, errorThrown) {
 				
-			},
-			timeout: 120000
-});
+				// Create questStep objects for each quest step and add to questSteps array
+				for (j = 0; j < Object.keys(steps).length; j++) {
+					questSteps[j] = steps[j];
+				}
+				
+				// Create quest object for each quest and add to questJournal array
+				questJournal[i] = new quest(type, title, description, questItems, status, questSteps, i);
+			}
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			
+		},
+		timeout: 120000
+	});
+}
